73b14b985bdd86e74bb6ebb1d2ac408d49d05e6d
Juhani Mäkelä <ext-Juhani.3.Makela@nokia.com>
2008-11-05 12:00:44 +0200

    It's not necessary to bind sending UDP socket

--- lib/security/dlog_send.c
+++ lib/security/dlog_send.c
@@ -7,6 +7,8 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdarg.h>
+#include <syslog.h>
+#include <errno.h>
 
 #define DLOG_PORT htons(2300)
 #define INET4A(a,b,c,d) (in_addr_t)htonl(a << 24 | b << 16 | c << 8 | d)
@@ -27,28 +29,42 @@ dlog_message(const char* format, ...)
 
 	if (dlog_socket == -1) {
 		dlog_socket = socket(PF_INET, SOCK_DGRAM, 0);
-		if (dlog_socket < 0)
+		if (dlog_socket < 0) {
+			syslog(LOG_ERR, "%s(%d)[%s]: ERROR cannot create debug socket (%d)\n",
+				   __FILE__, __LINE__, __func__, errno);
 			return;
-		i_mad.sin_family = i_rad.sin_family = AF_INET;
+		}
+#if 0
+		i_mad.sin_family = AF_INET;
 		i_mad.sin_addr.s_addr = INADDR_ANY;
 		i_mad.sin_port = 0;
-		/*
-		 * TODO: Obviously...
-		 */
-		i_rad.sin_addr.s_addr = INET4A(192,168,2,1);
-		i_rad.sin_port = DLOG_PORT;
 		rc = bind(dlog_socket, (struct sockaddr*)&i_mad, sizeof(struct sockaddr_in));
 		if (rc < 0) {
+			syslog(LOG_ERR, "%s(%d)[%s]: ERROR cannot bind debug socket (%d)\n",
+				   __FILE__, __LINE__, __func__, errno);
 			close(dlog_socket);
 			dlog_socket = -1;
 			return;
+		} else {
+			syslog(LOG_INFO, "%s(%d)[%s]: bound debug socket to port %d\n",
+				   __FILE__, __LINE__, __func__, ntohs(i_mad.sin_port));
 		}
+#endif
 	}
-	
+	/*
+	 * TODO: Obviously...
+	 */
+	i_rad.sin_family = AF_INET;
+	i_rad.sin_addr.s_addr = INET4A(192,168,2,1);
+	i_rad.sin_port = DLOG_PORT;
 	va_start(p_arg, format);
 	printed = vsnprintf(sndbuf, sizeof(sndbuf) - 1, format, p_arg);
 	va_end(p_arg);
 	rc = sendto(dlog_socket, sndbuf, printed, MSG_DONTWAIT, 
 				(struct sockaddr*)&i_rad, 
 				sizeof(struct sockaddr_in));
+	if (rc < 0) {
+		syslog(LOG_ERR, "%s(%d)[%s]: ERROR cannot send debug message (%d)\n",
+			   __FILE__, __LINE__, __func__, errno);
+	}
 }
